cfa2bd4a43a72f7de4cdee9c63fec02b56ab940e
Erick Contreras <eri89--@hotmail.com>
2023-03-13 17:21:42 -0600

    Add Login Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './styles/App.css';
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Content from './components/Content';
+import LoginPage from './components/Login';
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const jwt = localStorage.getItem('jwt');
+    if (jwt) {
+      setAuthenticated(true);
+    }
+  }, []);
+
   return (
     <>
       <Header />
-      <Content />
+      {authenticated ? (
+        <Content to="/" />
+      ) : (
+        <LoginPage setAuthenticated={setAuthenticated} />
+      )}
     </>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+
+function LoginPage(setAuthenticated) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('/auth/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        localStorage.setItem('jwt', data.jwt);
+        setAuthenticated(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <h1>Please login first</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">
+            Username or Email
+            <input type="email" id="email" value={email} onChange={handleEmailChange} />
+          </label>
+
+          <label htmlFor="password">
+            Password
+            <input type="text" id="password" value={password} onChange={handlePasswordChange} />
+          </label>
+
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default LoginPage;
