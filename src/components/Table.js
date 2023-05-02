c5b12d27aa6db9ff889fdd2eebf58e6f1a7246fc
Erick Contreras <er.contreras@hotmail.com>
2023-05-02 16:05:44 -0600

    Get users by requesting to database and decoded token to get user_id and match only the bills with his respective user

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,6 +3,37 @@ import { useEffect, useState } from 'react';
 
 const Table = () => {
   const [bills, setBills] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      fetch('http://localhost:3000/users', {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+      })
+        .then((response) => response.json())
+        .then((users) => {
+          setUsers(users);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      window.location.href = '/login';
+    }
+  }, []);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      const decodedToken = atob(token.split('.')[1]);
+      const decodedUserId = JSON.parse(decodedToken);
+
+      const matchingCurrentUser = users.find((user) => user.id === decodedUserId.user_id);
+      setCurrentUser(matchingCurrentUser);
+    }
+  }, [users]);
 
   useEffect(() => {
     fetch('http://localhost:3000/bills', {
@@ -24,7 +55,7 @@ const Table = () => {
           </tr>
         </thead>
         <tbody>
-          {bills.map((row) => (
+          {bills.filter((bill) => bill.user_id === currentUser?.id).map((row) => (
             <tr key={uuidv4()}>
               <td>{row.name}</td>
               <td>{row.date}</td>
