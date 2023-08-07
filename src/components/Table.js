83be7a37492fab206c5a31decfa6ff86d1784a27
Erick Contreras <er.contreras@hotmail.com>
2023-08-06 18:58:38 -0600

    remove FetchUser function and currentUser validation from token

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,29 +1,11 @@
 import { v4 as uuidv4 } from 'uuid';
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaTrash, FaEdit, FaBell } from 'react-icons/fa';
 import PropTypes from 'prop-types';
-import FetchUser from './FetchUser';
 
 const Table = (props) => {
   const { bills, handleDelete } = props;
-  const [users, setUsers] = useState([]);
-  const [currentUser, setCurrentUser] = useState(null);
   const navigate = useNavigate();
-
-  FetchUser({ setUsers });
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-
-    if (token) {
-      const decodedToken = atob(token.split('.')[1]);
-      const decodedUserId = JSON.parse(decodedToken);
-
-      const matchingCurrentUser = users.find((user) => user.id === decodedUserId.user_id);
-      setCurrentUser(matchingCurrentUser);
-    }
-  }, [users]);
 
   const handleNotification = (name, id, date) => {
     fetch('http://localhost:3000/bill_notification', {
