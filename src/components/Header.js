7ba63d1729b223a6442a2ea6d22d52d057eb1eb5
Erick Contreras <er.contreras@hotmail.com>
2023-08-14 17:15:46 -0600

    Refactor: remove LogOut and CurrentUser and implment them into Header component directly

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,42 @@
-import CurrentUser from './CurrentUser';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Header = () => (
-  <header className="header">
-    <h1>Pay bills on time</h1>
-    <CurrentUser />
-  </header>
-);
+const Header = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+    const parsedUser = JSON.parse(user);
+
+    if (user) {
+      setCurrentUser(parsedUser);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    setCurrentUser(null);
+    navigate('/login');
+  };
+
+  return (
+    <header className="header">
+      <h1>Pay bills on time</h1>
+      <div className="user-info">
+        {currentUser && (
+          <div className="current-user">
+            Welcome
+            {' '}
+            {currentUser.username}
+            {' '}
+            <button type="button" onClick={handleLogout}>Logout</button>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
 
 export default Header;
