da6d17e6bf0f19a4a80844ccb87691dbd8207be6
Erick Contreras <er.contreras@hotmail.com>
2023-09-18 16:42:24 -0600

    Refactor: remove current user in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,11 @@
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-  const [currentUser, setCurrentUser] = useState(null);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const user = localStorage.getItem('user');
-    const parsedUser = JSON.parse(user);
-
-    if (user) {
-      setCurrentUser(parsedUser);
-    }
-  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
-    setCurrentUser(null);
     navigate('/login');
   };
 
@@ -25,16 +13,7 @@ const Header = () => {
     <header className="header">
       <h1>Pay bills on time</h1>
       <div className="user-info">
-        {currentUser && (
-          <div className="current-user">
-            Welcome
-            {' '}
-            {currentUser.username}
-            !
-            {' '}
-            <button type="button" onClick={handleLogout}>Logout</button>
-          </div>
-        )}
+        <button type="button" onClick={handleLogout}>Sign Out</button>
       </div>
     </header>
   );
